19d8594e5731cfb2496d8cf4bd44ad250a0f50fd
Alyson Vasconcelos <alysonlv@gmail.com>
2018-07-27 18:01:57 -0300

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,7 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://admin:alvascon8@ds247191.mlab.com:47191/alv_blog_dev',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
  
