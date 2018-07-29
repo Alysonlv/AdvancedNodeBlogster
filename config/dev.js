3876e9574e47476cda50e6b62afce636f5669164
Alyson Vasconcelos <alysonlv@gmail.com>
2018-07-29 16:38:28 -0300

    configurando aws s3

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,8 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://admin:alvascon8@ds247191.mlab.com:47191/alv_blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'asadasdasd',
+  secrectAccessKey: 'asdasdedasawd3ewd3e'
 };
  
